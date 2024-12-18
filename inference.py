922d9b3b17b31a526e402edbe26b7a07170fab24
Yiting1022 <yiting911022@gmail.com>
2024-12-18 23:27:45 +0800

    inference

--- inference.py
+++ inference.py
@@ -1,11 +1,12 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
-
+import json
+import csv
 # 設定裝置
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def inference_single_entry(model, tokenizer, entry, max_length=256):
+def inference_entry(model, tokenizer, entry, max_length=512):
     """
     單一推論方法，處理單筆資料，並生成模型的回應。
     """
@@ -13,7 +14,7 @@ def inference_single_entry(model, tokenizer, entry, max_length=256):
     input_text = entry["input"]
     
     # 構建輸入
-    formatted_input = f"{instruction}\\n{input_text}\\n"
+    formatted_input = f"{instruction}\n{input_text}"
     encoded_input = tokenizer(
         formatted_input, 
         return_tensors="pt", 
@@ -28,7 +29,7 @@ def inference_single_entry(model, tokenizer, entry, max_length=256):
         output_ids = model.generate(
             input_ids=input_ids, 
             attention_mask=attention_mask,
-            max_new_tokens=50, 
+            max_new_tokens=70, 
             pad_token_id=tokenizer.eos_token_id
         )
     output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
@@ -44,18 +45,36 @@ if __name__ == "__main__":
     
     model = AutoModelForCausalLM.from_pretrained(base_model_path, torch_dtype=torch.bfloat16).to(device)
     tokenizer = AutoTokenizer.from_pretrained(base_model_path)
-
-    test_entry = {
-        "instruction": "根據以下對話生成適當的回應。",
-        "input": "你再這樣下去，大家會把你當成公司的遲刻魔\n",
-    }
-    
-    # 推論
-    model.eval()
-    generated_response = inference_single_entry(model, tokenizer, test_entry)
-    print("Without LoRA:\n", generated_response)
     model = PeftModel.from_pretrained(model, peft_path)
-    model = model.to(device)
-    generated_response = inference_single_entry(model, tokenizer, test_entry)
-    print("With LoRA:\n", generated_response)
+    model.eval()
+
+    with open("retrieval/keyword_query.json") as f:
+        test_data = json.load(f)
     
+    with open("raw_dataset/活網用語辭典.csv", newline="") as f:
+        reader = csv.DictReader(f)
+        keyword_list = [(row["詞彙"],row["釋義"]) for row in reader]
+
+    for d in test_data:
+        for entry in d["transcriptions"]:
+
+            conversation = entry["A"]
+            text = conversation["text"]
+            best_matches_ids = [id["matched_index"] for id in conversation["top_matches"]]
+            best_matches_words = [keyword_list[i][0] for i in best_matches_ids]
+            best_matches_def = [keyword_list[i][1] for i in best_matches_ids]
+
+            # 組合詞彙和釋義為詞語定義
+            definitions = "\n".join(
+                [f"詞語定義{i+1}: {word} - {definition}" for i, (word, definition) in enumerate(zip(best_matches_words, best_matches_def))]
+            )
+            input_text = f"{definitions}\nA: {text}\nB: "
+
+            #print("Input:\n", input_text)
+            test_entry = {
+                "instruction": "根據以下對話生成適當的回應。",
+                "input": input_text
+            }
+            print("Input:\nA: ", text)
+            response = inference_entry(model, tokenizer, test_entry).split("B: ")[1]  
+            print("Response:\nB: ", response)
